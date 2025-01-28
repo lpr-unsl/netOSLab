afa141639a3e3449c8117da9c096ddfc53c82949
cartaffe <cartaffe@frm.utn.edu.ar>
2025-01-28 14:10:01 -0300

    improved Virtual Box path

--- provision/README.md
+++ provision/README.md
@@ -1,9 +1,11 @@
 Requirements:
 - Virtualbox 7.0 or higher installed
-- git clone this_repo to Virtualbox directory (e.g. cd /home/user_name/VirtualBox\ VMs/)
+- git clone this_repo
 - Download  debian-12.6.0-amd64-netinst.iso from https://www.debian.org/distrib/
+- cd provision
 - run create_vm.sh
 -
 - Boot VM and select Advanced options -> Automated install
 -
-- https://raw.githubusercontent.com/lpr-unsl/netOSLab-os/refs/heads/main/provision/preseed.cfg
+-preseeded file is located at:
+- https://raw.githubusercontent.com/lpr-unsl/netOSLab/refs/heads/main/provision/preseed.cfg
--- provision/create_vm.sh
+++ provision/create_vm.sh
@@ -9,8 +9,9 @@ fi
 # Assign arguments to variables
 INTERFACE="$1"
 IMAGE_NAME="$2"
+VBPATH=$HOME"/VirtualBox VMs"
 
-VBoxManage createvm --name base --ostype "Debian_64" --register --basefolder .
+VBoxManage createvm --name base --ostype "Debian_64" --register --basefolder "$VBPATH"
 VBoxManage modifyvm base --memory 1024 --vram 128
 VBoxManage modifyvm base --ioapic on
 #VBoxManage modifyvm base --nic1 bridged --bridgeadapter1 wlp0s20f3
@@ -22,8 +23,8 @@ VBoxManage storagectl base --name "IDE Controller" --add ide --controller PIIX4
 VBoxManage storagectl base --name "SATA Controller" --add sata --controller IntelAhci
 VBoxManage storagectl base --name "Floppy Controller" --add floppy
 
-VBoxManage createhd --filename ./base/base_disk.vdi --size 200000 --format VDI
-VBoxManage storageattach base --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  ./base/base_disk.vdi
+VBoxManage createhd --filename "$VBPATH"/base/base_disk.vdi --size 200000 --format VDI
+VBoxManage storageattach base --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VBPATH"/base/base_disk.vdi
 #VBoxManage storageattach base --storagectl "Floppy Controller" --port 0 --device 0 --type fdd --medium ./NewFloppyDisk.img
 #VBoxManage storageattach base --storagectl "SATA Controller" --port 0 --device 1 --type hdd --medium  ./preseeded.vdi
 #VBoxManage storageattach base --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium ./debian-12.6.0-amd64-netinst.iso
